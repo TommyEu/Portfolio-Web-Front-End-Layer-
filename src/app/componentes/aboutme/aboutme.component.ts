fb1c5ea8fe8ec2a71d0f31e618d0224f467bd291
Tomás Lautaro Euliarte <tomaslautaroeuliarte@gmail.com>
2023-03-20 13:18:26 -0300

    implementado el backend

--- src/app/componentes/aboutme/aboutme.component.ts
+++ src/app/componentes/aboutme/aboutme.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Persona } from 'src/app/model/model/Persona.model';
+import { PersonaService } from 'src/app/servicios/persona.service';
 
 @Component({
   selector: 'app-aboutme',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutme.component.css']
 })
 export class AboutmeComponent implements OnInit {
-
-  constructor() { }
+  persona: Persona = new Persona(0,"","","","","","");//Lo inicializamos con sus datos nulos
+  constructor(public personaService:PersonaService) { }
 
   ngOnInit(): void {
+    this.personaService.traerInfo().subscribe(data => {this.persona = data});
   }
 
 }
